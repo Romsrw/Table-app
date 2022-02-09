7d0313b6b039ec35badcbb7bc4b034d1ebe5665c
Roma <rmahsurw@gmail.com>
2022-02-10 02:58:20 +0500

    modified select

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ const App = () => {
   );
 
   console.log(data);
+  console.log(selectOptions);
 
   return (
     <div className="App">
--- src/components/actionSelect/Select.jsx
+++ src/components/actionSelect/Select.jsx
@@ -1,7 +1,24 @@
 import Arrow from "../../UI/Arrow";
 import "./Select.css";
+import { useState } from "react";
 
-const Select = () => {
-  return <div className="selectWrapper"><Arrow /></div>;
+const Select = ({ options }) => {
+  const [isOpenSelect, setIsOpenSelect] = useState(false);
+  return (
+    <div className="selectWrapper">
+      <button onClick={() => setIsOpenSelect((prev) => !prev)}>
+        <Arrow />
+      </button>
+      {isOpenSelect && (
+        <div className="select">
+          <div className="select__option">
+            {options.map((option) => (
+              <div>{option}</div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 export default Select;
